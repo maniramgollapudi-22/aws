efa7a4aa27043c811b4753391a4fd54570f6d6c4
maniramgollapudi-22 <maniramgollapudi@gmail.com>
2025-09-25 14:00:52 +0530

    Update productService.js

--- docker-ecommerce-frontend/src/services/productService.js
+++ docker-ecommerce-frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8081/api/products";  // Backend API URL
+const API_BASE_URL = "ec2-13-218-89-137.compute-1.amazonaws.com";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://localhost:8080/api/products/images/${imagePath}`;
+  return `ec2-13-218-89-137.compute-1.amazonaws.com/api/products/images/${imagePath}`;
 };
